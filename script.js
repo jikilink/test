5a380447e45ede85e2f60e8d40cd4bba14030416
Nikita <example@gmail.com>
2021-01-03 19:29:28 +0300

    Это первый коммит

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//console.log("arr" + " - object");
+//console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log("2" == 2);
+
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose);
+console.log(5 ** 2);
